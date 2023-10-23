fff058f8ccea2aeed86b3eed79527439bc5f58b6
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-10-22 22:30:47 -0400

    src file

--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -97,7 +97,6 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                             }
 
                              */
-                            //ClearState currentState = clearViewModel;
                         }
                     }
                 }
